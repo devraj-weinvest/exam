b31e0dbbc3d6b15ed73634a5ee7c1588c95b3618
Devraj <devaraju.railsfactory@weinvest.net>
2019-03-23 13:46:11 +0530

    updated coverage

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,81 @@
 
 ActiveRecord::Schema.define(version: 20190322225259) do
 
+  create_table "answerd_questions", force: :cascade do |t|
+    t.integer  "question_id"
+    t.boolean  "answerd",       default: false
+    t.boolean  "correct"
+    t.integer  "user_id"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "actual_answer"
+    t.index ["question_id"], name: "index_answerd_questions_on_question_id"
+    t.index ["user_id"], name: "index_answerd_questions_on_user_id"
+  end
+
+  create_table "chapters", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_chapters_on_topic_id"
+  end
+
+  create_table "exams", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "question"
+    t.integer  "chapter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text     "options"
+    t.string   "answer"
+    t.index ["chapter_id"], name: "index_questions_on_chapter_id"
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "exam_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["exam_id"], name: "index_subjects_on_exam_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "subject_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subject_id"], name: "index_topics_on_subject_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
+    t.string   "encrypted_password",     default: "",      null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean  "allow_password_change",  default: false
+    t.datetime "remember_created_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "image"
+    t.string   "email"
+    t.text     "tokens"
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
 end
